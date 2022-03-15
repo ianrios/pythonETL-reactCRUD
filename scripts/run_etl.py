3de3ce30df1144d3bc3da95ba4a5594926ba98a3
Kevin Lin <klin@nygenome.org>
2022-03-15 15:41:36 +0000

    Updated etl script criteria

--- scripts/run_etl.py
+++ scripts/run_etl.py
@@ -1,6 +1,4 @@
-import csv
 import logging
-import shutil
 import sys
 from time import perf_counter
 from pydantic import BaseModel
@@ -28,10 +26,18 @@ class CrimeDataRecord(BaseModel):
     def to_json(self) -> str:
         return self.json()
 
+
+class CrimeTypeMetrics(BaseModel):
+    primary_type: str
+    arrest_count: int  # The count of all CrimeDataRecords for a primary_type where arrest == True.
+    non_arrest_count: int  # The count of all CrimeDataRecords for a primary_type where arrest == False.
+
+
 input_csv = Path("data/crime.csv")
 outputs_dir = Path(".outputs")
 
-def parse():
+
+def parse() -> list[CrimeTypeMetrics]:
     """YOUR CODE GOES IN THIS FUNCTION.
 
     Assignment:
@@ -39,7 +45,7 @@ def parse():
         - Update this parse() function to:
             - Read the data from "data/crime.csv" and parse all the data rows into the CrimeDataRecord class above.
             - Convert each CrimeDataRecord to JSON and output it to a corresponding text file in a directory called ".outputs".
-            Each JSON string should be written to a single line in the corresponding file. The model has a ".to_json()" method 
+            Each JSON string should be written to a single line in the corresponding file. The model has a ".to_json()" method
             to simplify conversion for you. Example:
                 - outputs/
                     - PUBLIC PEACE VIOLATION.txt
@@ -47,7 +53,8 @@ def parse():
                     - CRIMINAL DAMAGE.txt
                     - NARCOTICS.txt
                     - etc...
-            - Return a list of primary_type values, sorted from most frequently occurring to least.
+            - Return a list of CrimeTypeMetric values, sorted from most frequently occurring to least.
+                Occurrences should be calculated based on the number of CrimeTypeData records corresponding to the primary_type.
 
     Success Criteria:
         - Running this python module executes the parsing logic and test logic successfully without any errors.
@@ -61,12 +68,12 @@ def parse():
         been done.
     """
 
-    pass
+    return []
 
 
 def check_criteria(test: Callable, name: str):
     """DO NOT CHANGE THIS."""
-    
+
     try:
         test()
         logger.info(f"Test [{name}]: PASSED")
@@ -75,49 +82,59 @@ def check_criteria(test: Callable, name: str):
         raise
 
 
-def test_crime_type_ordering(crime_types: list[str]):
+def test_crime_type_ordering(crime_metrics: list[CrimeTypeMetrics]):
     """DO NOT CHANGE THIS."""
 
-    expected_ordered_crime_types = [
-        "THEFT",
-        "BATTERY",
-        "CRIMINAL DAMAGE",
-        "ASSAULT",
-        "BURGLARY",
-        "OTHER OFFENSE",
-        "DECEPTIVE PRACTICE",
-        "NARCOTICS",
-        "MOTOR VEHICLE THEFT",
-        "ROBBERY",
-        "CRIMINAL TRESPASS",
-        "OFFENSE INVOLVING CHILDREN",
-        "WEAPONS VIOLATION",
-        "PUBLIC PEACE VIOLATION",
-        "SEX OFFENSE",
-        "CRIM SEXUAL ASSAULT",
-        "LIQUOR LAW VIOLATION",
-        "ARSON",
-        "PROSTITUTION",
-        "KIDNAPPING",
-        "INTERFERENCE WITH PUBLIC OFFICER",
-        "GAMBLING",
-        "HOMICIDE",
-        "INTIMIDATION",
-        "STALKING",
-        "CRIMINAL SEXUAL ASSAULT",
-        "OBSCENITY",
-        "CONCEALED CARRY LICENSE VIOLATION",
-        "NON-CRIMINAL",
-        "OTHER NARCOTIC VIOLATION",
-        "PUBLIC INDECENCY",
-        "HUMAN TRAFFICKING",
-        "NON - CRIMINAL",
-        "RITUALISM",
-        "NON-CRIMINAL (SUBJECT SPECIFIED)",
+    expected_ordered_crime_metrics = [
+        ["THEFT", 9689, 96847],
+        ["BATTERY", 19514, 67270],
+        ["CRIMINAL DAMAGE", 3586, 55385],
+        ["ASSAULT", 7432, 28653],
+        ["BURGLARY", 1823, 32026],
+        ["OTHER OFFENSE", 4851, 27937],
+        ["DECEPTIVE PRACTICE", 4381, 24413],
+        ["NARCOTICS", 27876, 284],
+        ["MOTOR VEHICLE THEFT", 2123, 25239],
+        ["ROBBERY", 1924, 15515],
+        ["CRIMINAL TRESPASS", 7687, 4812],
+        ["OFFENSE INVOLVING CHILDREN", 1033, 4727],
+        ["WEAPONS VIOLATION", 3799, 1812],
+        ["PUBLIC PEACE VIOLATION", 2293, 2035],
+        ["SEX OFFENSE", 958, 2137],
+        ["CRIM SEXUAL ASSAULT", 429, 1860],
+        ["LIQUOR LAW VIOLATION", 1583, 11],
+        ["ARSON", 211, 1154],
+        ["PROSTITUTION", 1323, 6],
+        ["KIDNAPPING", 106, 1051],
+        ["INTERFERENCE WITH PUBLIC OFFICER", 933, 126],
+        ["GAMBLING", 872, 6],
+        ["HOMICIDE", 389, 340],
+        ["INTIMIDATION", 97, 445],
+        ["STALKING", 80, 427],
+        ["CRIMINAL SEXUAL ASSAULT", 24, 260],
+        ["OBSCENITY", 71, 20],
+        ["CONCEALED CARRY LICENSE VIOLATION", 38, 0],
+        ["NON-CRIMINAL", 1, 17],
+        ["OTHER NARCOTIC VIOLATION", 15, 3],
+        ["PUBLIC INDECENCY", 18, 0],
+        ["HUMAN TRAFFICKING", 1, 8],
+        ["NON - CRIMINAL", 2, 5],
+        ["RITUALISM", 0, 5],
+        ["NON-CRIMINAL (SUBJECT SPECIFIED)", 1, 1],
     ]
-    assert (
-        ordered_crime_types == expected_ordered_crime_types
-    ), "The crime parsed types are not ordered in order of most to least frequently occurring."
+    assert len(crime_metrics) == len(
+        expected_ordered_crime_metrics
+    ), "The list of metrics do not match the expected count."
+    for i in range(0, len(crime_metrics)):
+        assert (
+            crime_metrics[i].primary_type == expected_ordered_crime_metrics[i][0]
+        ), f"The metric at output index {i} did not match the expected value"
+        assert (
+            crime_metrics[i].arrest_count == expected_ordered_crime_metrics[i][1]
+        ), f"The metric at output index {i} did not match the expected value"
+        assert (
+            crime_metrics[i].non_arrest_count == expected_ordered_crime_metrics[i][2]
+        ), f"The metric at output index {i} did not match the expected value"
 
 
 def test_output_files(output_path: Path):
@@ -142,13 +159,13 @@ if __name__ == "__main__":
 
     logger.info("Executing parse() method.")
     t1 = perf_counter()
-    ordered_crime_types = parse()
+    output = parse()
     t2 = perf_counter()
     logger.info(f"Completed parsing in {timedelta(seconds=t2-t1)}.")
 
     check_criteria(
-        lambda: test_crime_type_ordering(crime_types=ordered_crime_types),
-        "Returned crime types are ordered by frequency",
+        lambda: test_crime_type_ordering(crime_metrics=output),
+        "Crime type metrics are aggregated and sorted by frequency",
     )
     check_criteria(
         lambda: test_output_files(output_path=outputs_dir),
