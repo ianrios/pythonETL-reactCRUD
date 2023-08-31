b809cda6d2bc454491168b069253da715c520199
Ian Rios <ian.rios1a@gmail.com>
2023-08-31 22:14:24 +0000

    started work on react app

---
+++ ui/src/app2.js
@@ -0,0 +1,12 @@
+import Button from '@mui/material/Button';
+
+function App() {
+  return (
+    <div className="App">
+     app
+     <Button variant="contained">Hello world</Button>
+    </div>
+  );
+}
+
+export default App;
